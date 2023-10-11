c9b0f08d97cf37af38c9f6a0e01b73b6771d6d08
Afram94 <afram.h@hotmail.com>
2023-10-11 23:51:01 +0200

    Fixed sidebar animation with Tailwind, improved appearance, and added global CSS file

--- resources/js/Components/Sidebar.tsx
+++ resources/js/Components/Sidebar.tsx
@@ -1,6 +1,7 @@
-import { Link } from '@inertiajs/react';
 import React from 'react';
-import { FaHome, FaUser, FaCog } from 'react-icons/fa';
+import { Link } from '@inertiajs/react';
+
+import { FaHome, FaUser, FaCog, FaBars } from 'react-icons/fa';
 
 // Type definition for the Sidebar props
 type SidebarProps = {
@@ -24,26 +25,25 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, toggleSidebar }) => {
   return (
     <div 
       // Dynamic class assignment for the sidebar. If isOpen is true, it's wider; otherwise, it's narrower.
-      className={`transition-transform duration-300 ease-in-out ${isOpen ? 'w-64' : 'w-16'} bg-gray-700 h-full p-4`}
-    >
-      <div 
-        className='flex justify-center font-bold text-white uppercase text-md cursor-pointer' 
-        onClick={toggleSidebar} // On click, it will toggle the sidebar's open/close state
+      className={`transition-all duration-700 ease-in-out ${isOpen ? 'w-64' : 'w-20'} bg-gray-700 h-full p-4`}
       >
-        Crm 
-        {/* Conditionally render the "-System" part based on the isOpen value */}
-        <p className={ isOpen ? " " : "hidden" }> -System</p>
+      <div onClick={toggleSidebar} className='flex justify-center cursor-pointer'>
+        <FaBars size={24} className='text-white' />
       </div>
+
       <div className='mt-4'>
         {/* Map over the pages array to dynamically render the links */}
         {pages.map((page) => (
           <Link key={page.name} href={page.path}>
-            <div className={`mb-4 flex items-center w-full ${page.path === currentPath ? 'bg-gray-200 text-gray-800 rounded-lg p-2' : 'text-white'}`}> 
-              {/* Render the appropriate icon for the page */}
-              <page.icon className={`${page.path === currentPath ? ' text-gray-800 ' : 'text-white'}`} size={24}/>
-              {/* If the sidebar is open, also display the name of the page */}
+            <div className={`mb-4 flex items-center w-full 
+                ${isOpen ? '' : 'justify-center'} 
+                ${page.path === currentPath ? 'bg-gray-200 text-gray-800' : 'text-white hover:bg-gray-500 transition-bg duration-500 ease-in-out'} 
+                rounded-lg p-2
+              `}>
+              <page.icon className="" size={26}/>
               {isOpen && <p className="ml-4 ">{page.name}</p>}
             </div>
+
           </Link>
         ))}
       </div>
