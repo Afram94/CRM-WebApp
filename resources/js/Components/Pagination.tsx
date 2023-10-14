b3351ad91e71b23cbabb669025f7ffca81449983
Afram94 <afram.h@hotmail.com>
2023-10-14 23:16:06 +0200

    Adjusted the pagination to display only the first three pages and the last page, regardless of the total number of pages

--- resources/js/Components/Pagination.tsx
+++ resources/js/Components/Pagination.tsx
@@ -12,9 +12,24 @@ interface PaginationProps {
 }
 
 const PaginationComponent: React.FC<PaginationProps> = ({ links, onPageClick }) => {
+    // Extract numeric pages from links
+    const numericPages = links.filter(link => !isNaN(Number(link.label)));
+
+    // Filter the links to show first 5, and the last page
+    const filteredLinks = links.filter(link => {
+        if (link.label === "&laquo; Previous" || link.label === "Next &raquo;") return true;
+        if (numericPages.indexOf(link) < 3) return true; // first 5 pages
+        if (numericPages.indexOf(link) === numericPages.length - 1) return true; // last page
+        return false;
+    });
+
+    // Insert ellipsis if more than 6 links (5 pages + 2 for prev and next)
+    if (filteredLinks.length > 7) {
+        filteredLinks.splice(6, 0, { url: null, label: "...", active: false });
+    }
     return (
         <div className="mt-4 flex justify-center items-center space-x-4">
-            {links.map((link, index) => {
+            {filteredLinks.map((link, index) => {
                 let content;
                 if (link.label === "&laquo; Previous") {
                     content = "← Prev";
