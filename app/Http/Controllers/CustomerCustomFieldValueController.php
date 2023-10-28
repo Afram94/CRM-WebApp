1aadee623da9c160478c4b75413e8dbff53453dc
Afram94 <afram.h@hotmail.com>
2023-10-28 20:27:07 +0200

    Fix validation in create and update methods of CustomerCustomFieldValue

--- app/Http/Controllers/CustomerCustomFieldValueController.php
+++ app/Http/Controllers/CustomerCustomFieldValueController.php
@@ -6,12 +6,50 @@ use Illuminate\Http\Request;
 use App\Models\CustomerCustomFieldValue;
 use App\Models\Customer;
 use App\Models\CustomerCustomField;
+use Illuminate\Validation\ValidationException;
 
 class CustomerCustomFieldValueController extends Controller
 {
+    
+
     public function store(Request $request, $customerId)
     {
         $customFields = $request->input('custom_fields');
+
+        // Prepare validation rules dynamically based on custom fields
+        $validationRules = [];
+        foreach ($customFields as $fieldId => $value) {
+            $fieldDefinition = CustomerCustomField::find($fieldId);
+            
+            if (!$fieldDefinition) {
+                // Directly throw an error if the field ID is not found
+                throw ValidationException::withMessages([
+                    "custom_fields.$fieldId" => ["Field ID $fieldId not found"]
+                ]);
+            }
+
+            // Set validation rules based on the field's type
+            switch ($fieldDefinition->field_type) {
+                case 'string':
+                    $validationRules["custom_fields.$fieldId"] = 'required|string';
+                    break;
+                case 'integer':
+                    $validationRules["custom_fields.$fieldId"] = 'required|numeric';
+                    break;
+                case 'date':
+                    $validationRules["custom_fields.$fieldId"] = 'required|date';
+                    break;
+                case 'boolean':
+                    $validationRules["custom_fields.$fieldId"] = 'required|boolean';
+                    break;
+                // ... add other cases as needed
+                default:
+                    $validationRules["custom_fields.$fieldId"] = 'required|string';
+            }
+        }
+
+        // Validate the input
+        $request->validate($validationRules);
 
         // Fetch the customer from the database
         $customer = Customer::findOrFail($customerId);
@@ -21,15 +59,12 @@ class CustomerCustomFieldValueController extends Controller
             $customFieldValue->customer_id = $customerId;
             $customFieldValue->field_id = $fieldId; // Make sure to set this!
             $customFieldValue->value = $value;
-
-            // If you want to set the user_id, do it here
-            /* $customFieldValue->user_id = auth()->id(); */
-
             $customFieldValue->save();
         }
 
         return response()->json(['message' => 'Custom fields saved successfully']);
     }
+
 
 
     public function update(Request $request, $customerId)
@@ -39,31 +74,57 @@ class CustomerCustomFieldValueController extends Controller
 
         $customFieldValues = $request->input('custom_fields', []);
 
+        // Prepare validation rules dynamically based on custom fields
+        $validationRules = [];
         foreach ($customFieldValues as $id => $value) {
-            // Check if the field_id exists in customer_custom_fields
-            $fieldDefinitionExists = CustomerCustomField::find($id);
-
-            if (!$fieldDefinitionExists) {
-                return response()->json(['error' => "Field ID $id not found"], 400);
+            $fieldDefinition = CustomerCustomField::find($id);
+            
+            if (!$fieldDefinition) {
+                // Directly throw an error if the field ID is not found
+                throw ValidationException::withMessages([
+                    "custom_fields.$id" => ["Field ID $id not found"]
+                ]);
             }
 
+            // Set validation rules based on the field's type
+            switch ($fieldDefinition->field_type) {
+                case 'string':
+                    $validationRules["custom_fields.$id"] = 'required|string';
+                    break;
+                case 'integer':
+                    $validationRules["custom_fields.$id"] = 'required|numeric';
+                    break;
+                case 'date':
+                    $validationRules["custom_fields.$id"] = 'required|date';
+                    break;
+                case 'boolean':
+                    $validationRules["custom_fields.$fieldId"] = 'required|boolean';
+                    break;
+                // ... add other cases as needed
+                default:
+                    $validationRules["custom_fields.$id"] = 'required|string';
+            }
+        }
+
+        // Validate the input
+        $request->validate($validationRules);
+
+        foreach ($customFieldValues as $id => $value) {
             // Fetch or create a new field value based on customer_id and field_id
             $fieldValue = CustomerCustomFieldValue::firstOrNew([
                 'customer_id' => $customerId,
                 'field_id' => $id,
             ]);
 
-            //here if i want to make the value null or empty again
-            //check then if is good to have an empty value
+            // Set the value; you might want to modify this depending on your business logic
             $fieldValue->value = $value ?? '';  // If value is null, set to empty string
 
-
-            /* $fieldValue->value = $value; */
             $fieldValue->save();
         }
 
         return response()->json(['message' => 'Custom fields updated successfully']);
     }
+
 
 
 
