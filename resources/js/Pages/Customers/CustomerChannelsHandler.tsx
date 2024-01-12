9bbd9cf42773dda583e12c627fcdb0fde6927249
Afram94 <afram.h@hotmail.com>
2024-01-12 18:30:13 +0100

    Fixed duplicate data issue in customer and product creation via WebSockets for child users

--- resources/js/Pages/Customers/CustomerChannelsHandler.tsx
+++ resources/js/Pages/Customers/CustomerChannelsHandler.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useEcho } from '../../../providers/WebSocketContext';
 import { Customer } from '@/types';
 
@@ -8,92 +8,55 @@ interface CustomerChannelsHandlerProps {
     onNewCustomer: (customer: Customer) => void;
     onUpdateCustomer: (customer: Customer) => void;
     onDeleteCustomer: (customer: number) => void;
-    
 }
 
-const CustomerChannelsHandler: React.FC<CustomerChannelsHandlerProps> = ({userId, parentId, onNewCustomer, onUpdateCustomer, onDeleteCustomer }) => {
-  const echo = useEcho();
+const CustomerChannelsHandler: React.FC<CustomerChannelsHandlerProps> = ({
+    userId, parentId, onNewCustomer, onUpdateCustomer, onDeleteCustomer
+}) => {
+    const echo = useEcho();
 
+    useEffect(() => {
+        if (!echo || !userId) return;
 
-  useEffect(() => {
-    if (echo && userId) {
-        console.log("Create_1")
         const userChannel = echo.private(`customers-for-user-${userId}`)
-        .listen('CustomerCreated', (e: { customer: Customer }) => {
-          onNewCustomer(e.customer);
-        });
+            .listen('CustomerCreated', (e: { customer: Customer }) => {
+                onNewCustomer(e.customer);
+            })
+            .listen('CustomerUpdated', (e: { customer: Customer }) => {
+                onUpdateCustomer(e.customer);
+            })
+            .listen('CustomerDeleted', (e: { customerId: number }) => {
+                onDeleteCustomer(e.customerId);
+            });
 
         let parentChannel: any;
-      if (parentId) {
-        console.log("Create_2")
-        parentChannel = echo.private(`customers-for-user-${parentId}`)
-          .listen('CustomerCreated', (e: { customer: Customer }) => {
-            onNewCustomer(e.customer);
-          });
-      }
-
-      console.log("Update_1")
-        const updateUserChannel = echo.private(`customers-for-user-${userId}`)
-        .listen('CustomerUpdated', (e: { customer: Customer }) => {
-          onUpdateCustomer(e.customer);
-        });
-
-        let updateParentChannel: any;
-      if (parentId) {
-        console.log("Update_2")
-        updateParentChannel = echo.private(`customers-for-user-${parentId}`)
-          .listen('CustomerUpdated', (e: { customer: Customer }) => {
-            onUpdateCustomer(e.customer);
-          });
-      }
-
-
-      console.log("Delete_1")
-        const deleteUserChannel = echo.private(`customers-for-user-${userId}`)
-        .listen('CustomerDeleted', (e: { customerId: number }) => {
-          onDeleteCustomer(e.customerId);
-        });
-
-        let deleteParentChannel: any;
-      if (parentId) {
-        console.log("Delete_2")
-        deleteParentChannel = echo.private(`customers-for-user-${parentId}`)
-          .listen('CustomerDeleted', (e: { customerId: number }) => {
-            onDeleteCustomer(e.customerId);
-          });
-      }
-
-      
-
-      // Cleanup function to unsubscribe from channels
-      return () => {
-        console.log("clear_1")
-        userChannel.stopListening('CustomerCreated');
-        if (parentChannel) {
-          console.log("clear_2")
-          parentChannel.stopListening('CustomerCreated');
+        if (parentId && parentId !== userId) {
+            parentChannel = echo.private(`customers-for-user-${parentId}`)
+                .listen('CustomerCreated', (e: { customer: Customer }) => {
+                    onNewCustomer(e.customer);
+                })
+                .listen('CustomerUpdated', (e: { customer: Customer }) => {
+                    onUpdateCustomer(e.customer);
+                })
+                .listen('CustomerDeleted', (e: { customerId: number }) => {
+                    onDeleteCustomer(e.customerId);
+                });
         }
 
-        console.log("Update_clear_1")
-        updateUserChannel.stopListening('CustomerUpdated');
-        if (updateParentChannel) {
-          console.log("Update_clear_2")
-          updateParentChannel.stopListening('CustomerUpdated');
-        }
+        return () => {
+            userChannel.stopListening('CustomerCreated')
+                .stopListening('CustomerUpdated')
+                .stopListening('CustomerDeleted');
+            
+            if (parentChannel) {
+                parentChannel.stopListening('CustomerCreated')
+                    .stopListening('CustomerUpdated')
+                    .stopListening('CustomerDeleted');
+            }
+        };
+    }, [echo, userId, parentId, onNewCustomer, onUpdateCustomer, onDeleteCustomer]);
 
-        console.log("Delete_clear_1")
-        deleteUserChannel.stopListening('CustomerDeleted');
-        if (deleteParentChannel) {
-          console.log("Delete_clear_2")
-          deleteParentChannel.stopListening('CustomerDeleted');
-        }
-
-      };
-    }
-  }, [echo, userId, onNewCustomer]);
-
-  // This component doesn't render anything; it's just for setting up WebSocket subscriptions
-  return null;
+    return null; // This component does not render anything
 };
 
 export default CustomerChannelsHandler;
--- resources/js/Pages/Products/ProductChannelsHandler.tsx
+++ resources/js/Pages/Products/ProductChannelsHandler.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useEcho } from '../../../providers/WebSocketContext';
 import { Product } from '@/types';
 
@@ -8,92 +8,55 @@ interface ProductChannelsHandlerProps {
     onNewProduct: (product: Product) => void;
     onUpdateProduct: (product: Product) => void;
     onDeleteProduct: (product: number) => void;
-    
 }
 
-const ProductChannelsHandler: React.FC<ProductChannelsHandlerProps> = ({userId, parentId, onNewProduct, onUpdateProduct, onDeleteProduct }) => {
+const ProductChannelsHandler: React.FC<ProductChannelsHandlerProps> = ({
+    userId, parentId, onNewProduct, onUpdateProduct, onDeleteProduct
+}) => {
   const echo = useEcho();
 
-
   useEffect(() => {
-    if (echo && userId) {
-        console.log("Product_Create_1")
-        const userChannel = echo.private(`products-for-user-${userId}`)
+    if (!echo || !userId) return;
+
+    const userChannel = echo.private(`products-for-user-${userId}`)
+      .listen('ProductCreated', (e: { product: Product }) => {
+        onNewProduct(e.product);
+      })
+      .listen('ProductUpdated', (e: { product: Product }) => {
+        onUpdateProduct(e.product);
+      })
+      .listen('ProductDeleted', (e: { productId: number }) => {
+        onDeleteProduct(e.productId);
+      });
+
+    let parentChannel: any;
+    if (parentId && parentId !== userId) {
+      parentChannel = echo.private(`products-for-user-${parentId}`)
         .listen('ProductCreated', (e: { product: Product }) => {
           onNewProduct(e.product);
-        });
-
-        let parentChannel: any;
-      if (parentId) {
-        console.log("Product_Create_2")
-        parentChannel = echo.private(`products-for-user-${parentId}`)
-          .listen('ProductCreated', (e: { product: Product }) => {
-            onNewProduct(e.product);
-          });
-      }
-
-      console.log("Update_1")
-        const updateUserChannel = echo.private(`products-for-user-${userId}`)
+        })
         .listen('ProductUpdated', (e: { product: Product }) => {
           onUpdateProduct(e.product);
-        });
-
-        let updateParentChannel: any;
-      if (parentId) {
-        console.log("Update_2")
-        updateParentChannel = echo.private(`products-for-user-${parentId}`)
-          .listen('ProductUpdated', (e: { product: Product }) => {
-            onUpdateProduct(e.product);
-          });
-      }
-
-
-      console.log("Delete_1")
-        const deleteUserChannel = echo.private(`products-for-user-${userId}`)
+        })
         .listen('ProductDeleted', (e: { productId: number }) => {
           onDeleteProduct(e.productId);
         });
-
-        let deleteParentChannel: any;
-      if (parentId) {
-        console.log("Delete_2")
-        deleteParentChannel = echo.private(`products-for-user-${parentId}`)
-          .listen('ProductDeleted', (e: { productId: number }) => {
-            onDeleteProduct(e.productId);
-          });
-      }
-
-      
-
-      // Cleanup function to unsubscribe from channels
-      return () => {
-        console.log("product_clear_1")
-        userChannel.stopListening('ProductCreated');
-        if (parentChannel) {
-          console.log("product_clear_2")
-          parentChannel.stopListening('ProductCreated');
-        }
-
-        console.log("Update_clear_1")
-        updateUserChannel.stopListening('ProductUpdated');
-        if (updateParentChannel) {
-          console.log("Update_clear_2")
-          updateParentChannel.stopListening('ProductUpdated');
-        }
-
-        console.log("Delete_clear_1")
-        deleteUserChannel.stopListening('ProductDeleted');
-        if (deleteParentChannel) {
-          console.log("Delete_clear_2")
-          deleteParentChannel.stopListening('ProductDeleted');
-        }
-
-      };
     }
-  }, [echo, userId, onNewProduct]);
 
-  // This component doesn't render anything; it's just for setting up WebSocket subscriptions
-  return null;
+    return () => {
+      userChannel.stopListening('ProductCreated')
+        .stopListening('ProductUpdated')
+        .stopListening('ProductDeleted');
+      
+      if (parentChannel) {
+        parentChannel.stopListening('ProductCreated')
+          .stopListening('ProductUpdated')
+          .stopListening('ProductDeleted');
+      }
+    };
+  }, [echo, userId, parentId, onNewProduct, onUpdateProduct, onDeleteProduct]);
+
+  return null; // This component does not render anything
 };
 
 export default ProductChannelsHandler;
